f9e67f869bf38bcd9fd3bf8504314d44122c4fb6
Tim Kelly <63365669+timotheekelly@users.noreply.github.com>
2026-03-12 15:01:47 +0000

    Add MongoDB session configuration

--- src/main/java/com/mongodb/springsessionsmongodbapp/SessionConfig.java
+++ src/main/java/com/mongodb/springsessionsmongodbapp/SessionConfig.java
@@ -1,4 +1,9 @@
 package com.mongodb.springsessionsmongodbapp;
 
+import org.mongodb.spring.session.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMongoHttpSession
 public class SessionConfig {
 }
